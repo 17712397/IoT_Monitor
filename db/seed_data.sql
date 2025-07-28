a019a45bf5d7c406261ceade264982c2fb1eda67
YukiharuNagao <yukiharu.naga.cs@fujifilm.com>
2025-07-28 17:06:14 +0900

    基礎データクエリ作成

---
+++ db/seed_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO buildings (building_name) VALUES
+('配送センター'),
+('研究棟'),
+('品質管理棟'),
+('製剤工場'),
+('事務棟'),
+('厚生棟'),
+('試薬研究所'),
+('製品第二工場'),
+('製品工場');
+
+INSERT INTO measure_type (measure_type) VALUES
+('温度'),
+('湿度'),
+('電力');
